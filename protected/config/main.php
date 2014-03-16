309d83dcb0f1ec02e71ddd4e87a91c9f4ca95ea9
Rajat Deshpande <rajatdeshpande@gmail.com>
2014-03-16 14:08:49 +0530

    added landing page

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Student Loan',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -20,14 +20,14 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'gii',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		
 	),
 
 	// application components
@@ -72,7 +72,7 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				/*
+			/*	
 				array(
 					'class'=>'CWebLogRoute',
 				),
@@ -85,6 +85,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'rajatdeshpande@gmail.com',
 	),
 );
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,7 +3,7 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
-
+<!-- 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
@@ -11,10 +11,375 @@ $this->pageTitle=Yii::app()->name;
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
 	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
 </ul>
 
 <p>For more details on how to further develop this application, please read
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
 should you have any questions.</p>
+-->
+
+<!--=== Slider ===-->
+<div class="slider-inner">
+	<div id="da-slider" class="da-slider">
+		<div class="da-slide">
+			<h2>
+				<i>DON'T GET STUCK</i> <br /> <i>WITH THE STANDARD LOAN RATE</i><br />
+				<a href="loan_application.html" class="btn-u btn-u-lg btn-u-orange"><b>Find
+						My Rate</b> </a>
+				<!--<i>DESIGN</i>-->
+			</h2>
+			<!--<p><i>Lorem ipsum dolor amet</i> <br /> <i>tempor incididunt ut</i> <br /> <i>veniam omnis </i></p>-->
+			<!--<div class="da-img"><img src="assets/plugins/parallax-slider/img/1.png" alt="" /></div>-->
+			<!--<p><a href="#" class="btn-u btn-u-orange btn-u-large">FIND MY RATE</a></p>-->
+		</div>
+		<!--<div class="da-slide">
+            <h2><i>RESPONSIVE VIDEO</i> <br /> <i>SUPPORT AND</i> <br /> <i>MANY MORE</i></h2>
+            <p><i>Lorem ipsum dolor amet</i> <br /> <i>tempor incididunt ut</i></p>
+            <div class="da-img">
+				<iframe src="http://player.vimeo.com/video/47911018" width="530" height="300" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> 
+            </div>
+        </div>
+        <div class="da-slide">
+            <h2><i>USING BEST WEB</i> <br /> <i>SOLUTIONS WITH</i> <br /> <i>HTML5/CSS3</i></h2>
+            <p><i>Lorem ipsum dolor amet</i> <br /> <i>tempor incididunt ut</i> <br /> <i>veniam omnis </i></p>
+            <div class="da-img"><img src="assets/plugins/parallax-slider/img/html5andcss3.png" alt="image01" /></div>
+        </div>-->
+		<!--
+        <div class="da-arrows">
+            <span class="da-arrows-prev"></span>
+            <span class="da-arrows-next"></span>		
+        </div>-->
+	</div>
+	<!--/da-slider-->
+</div>
+<!--/slider-->
+<!--=== End Slider ===-->
+
+<!--=== Purchase Block ===-->
+<!--
+<div class="purchase">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <span>Unify is a clean and fully responsive incredible Template.</span>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi  vehicula sem ut volutpat. Ut non libero magna fusce condimentum eleifend enim a feugiat.</p>
+            </div>            
+            <div class="col-md-3">
+                <a href="#" class="btn-buy hover-effect">Purchase Now</a>            
+            </div>
+        </div>
+    </div>
+</div>-->
+<!--/row-->
+<!-- End Purchase Block -->
+
+<!--=== Content Part ===-->
+<div
+	class="container">
+	<!-- Service Blocks -->
+	<div class="row">
+		<div class="col-md-3">
+			<!--if using 3 items just change each class to 'col-md-4'-->
+			<div class="service">
+				<i class="icon-resize-small service-icon"></i>
+				<div class="desc">
+					<h4>More Savings</h4>
+					<p>Worried about your interest expenses going through the roof? Sit
+						back and let us find the best deal to refinance your loans.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="service">
+				<i class="icon-cogs service-icon"></i>
+				<div class="desc">
+					<h4>Superior Service</h4>
+					<p>Say goodbye to the frustrating paperwork for different lenders.
+						We will give you one rate and the best rate.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="service">
+				<i class="icon-plane service-icon"></i>
+				<div class="desc">
+					<h4>Career Support</h4>
+					<p>Take advantage of our growing community to explore career
+						opportunities and find the right job for yourself.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="service">
+				<i class="icon-plane service-icon"></i>
+				<div class="desc">
+					<h4>Anywhere Anytime</h4>
+					<p>Use our newly launched android app to find use or resources and
+						check your loan status on the go.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/row-->
+	<!-- End Service Blokcs -->
+
+	<!-- Recent Works -->
+	<div class="headline">
+		<h2>Read The Success Stories</h2>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-sm-6">
+			<div class="service">
+				<div class="thumbnail-img">
+					<div class="overflow-hidden">
+						<img class="img-responsive" src="assets/img/profiles/profile1.jpg"
+							alt="" />
+					</div>
+					<!--<a class="btn-more hover-effect" href="#">read more +</a>-->
+				</div>
+				<div class="caption">
+					<h3>
+						<a class="hover-effect" href="#">Amazing Service..</a>
+					</h3>
+					<p>
+						<i>"As a fresh graduate searching for a job, xxxx has helped me a
+							lot!"</i>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="service">
+				<div class="thumbnail-img">
+					<div class="overflow-hidden">
+						<img class="img-responsive"
+							src="assets/img/profiles/profile2.jpeg" alt="" />
+					</div>
+					<!--<a class="btn-more hover-effect" href="#">read more +</a>-->
+				</div>
+				<div class="caption">
+					<h3>
+						<a class="hover-effect" href="#">It was a pleasant surprise</a>
+					</h3>
+					<p>
+						<i>"Spoke to a community member who not only gave me invaluable
+							advice for managing my student finances and choosing the right
+							career."</i>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="service">
+				<div class="thumbnail-img">
+					<div class="overflow-hidden">
+						<img class="img-responsive" src="assets/img/profiles/profile3.jpg"
+							alt="" />
+					</div>
+					<!--<a class="btn-more hover-effect" href="#">read more +</a>-->
+				</div>
+				<div class="caption">
+					<h3>
+						<a class="hover-effect" href="#">Indispensible for a student..</a>
+					</h3>
+					<p>
+						<i>"I was worried about pursuing my entrepreneurship dreams post
+							college. XXXX helped me refinance with the right loan."</i>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/row-->
+	<!-- End Recent Works -->
+
+	<!-- Info Blokcs -->
+	<div class="row">
+		<!-- Welcome Block -->
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-sm-12" align=center>
+					<h3>
+						<b>Excited to Start Saving?</b>
+						</h4>
+
+						<div class="col-md-4"></div>
+						<div class="col-md-4" align=center>
+							<a href="#" class="btn-u btn-u-blue btn-u-large btn-block"><h4>Start
+									Now</h4> </a>
+						</div>
+						<div class="col-md-4"></div>
+				
+				</div>
+			</div>
+
+			<div class="col-md-12" align=center>
+				<div
+					class="servive-block-in servive-block-colored servive-block-grey">
+					<h4>
+						<b>Have any Questions?</b>
+					</h4>
+					<h5>
+						<b>Call us at : 1800-123-456</b>
+					</h5>
+				</div>
+			</div>
+		</div>
+		<!--/col-md-8-->
+
+		<!-- Latest Shots -->
+		<!--<div class="col-md-4">
+			<div class="headline"><h2>Latest Shots</h2></div>
+			<div id="myCarousel" class="carousel slide carousel-v1">
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="assets/img/main/5.jpg" alt="">
+                        <div class="carousel-caption">
+                            <p>Facilisis odio, dapibus ac justo acilisis gestinas.</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="assets/img/main/4.jpg" alt="">
+                        <div class="carousel-caption">
+                            <p>Cras justo odio, dapibus ac facilisis into egestas.</p>
+                        </div>
+                        </div>
+                    <div class="item">
+                        <img src="assets/img/main/3.jpg" alt="">
+                        <div class="carousel-caption">
+                            <p>Justo cras odio apibus ac afilisis lingestas de.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="carousel-arrow">
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <i class="icon-angle-left"></i>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <i class="icon-angle-right"></i>
+                    </a>
+                </div>
+			</div>
+        </div><!--/col-md-4-->
+	</div>
+	<!--/row-->
+	<!-- End Info Blokcs -->
+
+	<!-- Our Clients -->
+	<div id="clients-flexslider" class="flexslider home clients">
+		<div class="headline">
+			<h2>Our Partners</h2>
+		</div>
+		<ul class="slides">
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/hp_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/hp.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/igneus_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/igneus.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/vadafone_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/vadafone.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/walmart_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/walmart.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/shell_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/shell.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/natural_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/natural.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/aztec_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/aztec.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/gamescast_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/gamescast.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/cisco_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/cisco.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/everyday_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/everyday.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/cocacola_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/cocacola.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/spinworkx_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/spinworkx.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/shell_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/shell.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/natural_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/natural.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/gamescast_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/gamescast.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/everyday_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/everyday.png"
+					class="color-img" alt="" /> </a>
+			</li>
+			<li><a href="#"> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/spinworkx_grey.png"
+					alt="" /> <img
+					src="<?php echo Yii::app()->request->baseUrl; ?>/assets/img/clients/spinworkx.png"
+					class="color-img" alt="" /> </a>
+			</li>
+		</ul>
+	</div>
+	<!--/flexslider-->
+	<!-- End Our Clients -->
+</div>
+<!--/container-->
+<!-- End Content Part -->
